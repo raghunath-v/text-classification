8734a3a3e6714cdce15994d7348064f0ee805ada
Jihane Bouhammada <jihane.bouhammada@gmail.com>
2018-01-13 14:05:04 +0000

    SSK.py created online with Bitbucket

---
+++ SSK.py
@@ -0,0 +1,46 @@
+#author ='Jihane BOUHAMMADA'
+
+
+import numpy as np
+
+# Parameters :
+	# lambda_decay is the decay factor
+	# small k is the length of subsequences
+
+# computing K'
+def kPrime(k, s, t, lambda_decay=0.5):
+
+	if min(len(s), len(t)) < k:
+		return 0
+	else:
+		sum_ = 0
+		for j in range(1, len(t)):
+			if t[j] == s[-1]:
+				sum_ += kPrime(k-1, s[:-1], t[:j]) * (lambda_decay ** (len(t) - (j+1) + 2))
+		result = lambda_decay * kPrime(k, s[:-1], t) + sum_
+	return result
+
+
+# computing Kn
+def kKernel(k, s, t, lambda_decay=0.5):
+
+	if min(len(s), len(t)) < k:
+		return 0
+	else:
+		sum_ = 0
+		for j in range(1, len(t)):
+			if t[j] == s[-1]:
+				sum_ += kPrime(k-1, s[:-1], t[:j])
+		result = kKernel(k, s[:-1], t) + lambda_decay ** 2 * sum_
+	return result
+
+
+# computing SSK
+#def SSK(k, s, t, lambda_decay=0.5):
+
+
+lambda_decay = 0.5
+k = 2
+s = 'cat'
+t = 'car'
+print(kKernel(k, s, t))
